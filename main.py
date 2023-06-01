cf2e2286b3436a9a13ded88d8a7b59e9f0bbd5f6
MaLa1907 <132439182+MaLa1907@users.noreply.github.com>
2023-06-01 22:20:27 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+# import von internen Modulen (unsere Dateien)
+from gui_mainWindow import Window                       # Unsere "Window" Klasse, welches das Hauptfenster erstellt
+
+
+if __name__ == "__main__":
+    Window()
